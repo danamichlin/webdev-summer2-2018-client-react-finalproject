60ec340f091921c9f480b16544e0d9681d916307
Saikrishna Chintalpelly <saichintal@ccis.neu.edu>
2018-08-05 22:29:38 -0400

    Added images to file

--- src/index.js
+++ src/index.js
@@ -26,18 +26,40 @@ let store = createStore(widgetReducer);
 class WhiteBoard extends React.Component {
     constructor() {
         super();
-        this.state = {api: ''};
+        this.state = {api: '', images: []};
 
         this.titleChanged = this.titleChanged.bind(this);
+        this.renderImages = this.renderImages.bind(this); 
+        this.renderImageUrls = this.renderImageUrls.bind(this); 
         this.getAPI = this.getAPI.bind(this);
     }
     titleChanged(event) {
         this.setState({api: event.target.value})
     };
 
+    renderImages(response) {
+        response.items.forEach(element => {
+            this.setState({images: this.state.images.concat(element.snippet.thumbnails.default.url)})
+        });
+    };
+
+    renderImageUrls() {
+        var rows = this.state.images.map(function(image) {
+            console.log("rendering");
+            return <img className="widget-image" src={image} width="90"/>;
+        });
+        return (rows)
+    };
+
     getAPI() {
-        return window.location.replace("https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&q=" + this.state.api + "&type=video&key=AIzaSyBy8LJmSegON_gEweoYnun1JwZbzaojYhs")
-    }
+        this.setState({images: []});
+            fetch("https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&q=" + this.state.api + "&type=video&key=AIzaSyBy8LJmSegON_gEweoYnun1JwZbzaojYhs")
+                .then(function(response){
+                        return response.json(); 
+                    }).then(res => {
+                        this.renderImages(res);
+                    })
+        }; 
     
     render() {
         return (
@@ -51,6 +73,9 @@ class WhiteBoard extends React.Component {
                 <button onClick={this.getAPI}>
                                 UseApi
                             </button>
+                            <tbody>
+              {this.renderImageUrls()}
+            </tbody>
             </div>
             </Provider>
         )
