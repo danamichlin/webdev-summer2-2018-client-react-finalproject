cd4aa9ab12055a3391c323e8d5a9c5ef1d89132e
Saikrishna Chintalpelly <saichintal@ccis.neu.edu>
2018-08-05 22:02:08 -0400

    Added api call to youtube

--- src/index.js
+++ src/index.js
@@ -24,14 +24,33 @@ let store = createStore(widgetReducer);
 
 
 class WhiteBoard extends React.Component {
+    constructor() {
+        super();
+        this.state = {api: ''};
+
+        this.titleChanged = this.titleChanged.bind(this);
+        this.getAPI = this.getAPI.bind(this);
+    }
+    titleChanged(event) {
+        this.setState({api: event.target.value})
+    };
+
+    getAPI() {
+        return window.location.replace("https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&q=" + this.state.api + "&type=video&key=AIzaSyBy8LJmSegON_gEweoYnun1JwZbzaojYhs")
+    }
+    
     render() {
         return (
             <Provider store={store}>
             <div className="container-fluid" >
-                <div className="p-3 mb-2 bg-secondary text-white">Whiteboard</div>
-                <div>
-                    <CourseManager/>
-                </div>
+                <div className="p-3 mb-2 bg-secondary text-white">Recipe tutorials</div>
+                <input id="titleFld"
+                               placeholder="breaded chicken"
+                               onChange={this.titleChanged}
+                               />
+                <button onClick={this.getAPI}>
+                                UseApi
+                            </button>
             </div>
             </Provider>
         )
